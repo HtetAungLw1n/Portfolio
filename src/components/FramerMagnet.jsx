3b8e36bea181b4bf2f002211f73f1c27495339c7
Htet Aung Lwin <htetaunglwin.canon@gmail.com>
2025-02-24 18:09:57 +0630

    add framer motion and animation

---
+++ src/components/FramerMagnet.jsx
@@ -0,0 +1,37 @@
+import React, { useRef, useState } from "react";
+import { motion } from "framer-motion";
+
+const FramerMagnet = ({ children }) => {
+  const ref = useRef(null);
+  const [positon, setPosition] = useState({ x: 0, y: 0 });
+
+  const mouseMove = (e) => {
+    const { clientX, clientY } = e;
+    const { width, height, left, top } = ref.current.getBoundingClientRect();
+    const x = clientX - (left + width / 2);
+    const y = clientY - (top + height / 2);
+
+    setPosition({ x, y });
+  };
+
+  const mouseLeave = () => {
+    setPosition({ x: 0, y: 0 });
+  };
+
+  const { x, y } = positon;
+
+  return (
+    <motion.div
+      ref={ref}
+      onMouseMove={mouseMove}
+      onMouseLeave={mouseLeave}
+      animate={{ x, y }}
+      transition={{ type: "spring", stiffness: 150, damping: 15, mass: 0.1 }}
+      className=" text-mblue  float-start ml-10 w-32 h-32 rounded-full flex flex-col items-center justify-center border-2 border-mblue hover:bg-mblue hover:text-white-smoke transition-colors duration-200 ease-in-out"
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export default FramerMagnet;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,24 +1,47 @@
 import React from "react";
+import { motion } from "framer-motion";
+import FramerMagnet from "./FramerMagnet";
 
 const Hero = () => {
   return (
     <section className=" h-[40vh] md:h-[100vh] container mx-auto">
-      <div className="h-full flex flex-col justify-center items-center gap-8 md:gap-24 text-center md:text-start md:items-start">
+      <div className="h-full flex flex-col justify-center items-center gap-8 md:gap-20 text-center md:text-start md:items-start">
         <div className="text-2xl font-medium leading-8 md:text-5xl md:leading-16 tracking-normal md:tracking-wide md:mt-20">
-          <div>Bridging Design and Development</div>
-          <div>For Your Business Needs.</div>
+          <div className="overflow-hidden">
+            <motion.div
+              initial={{ y: "100%", opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ duration: 0.35 }}
+            >
+              Bridging Design and Development
+            </motion.div>
+          </div>
+          <div className="overflow-hidden">
+            <motion.div
+              initial={{ y: "100%", opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ duration: 0.35, delay: 0.1 }}
+            >
+              For Your Business Needs.
+            </motion.div>
+          </div>
         </div>
-        <div className="text-xs text-balance md:text-pretty md:text-xl md:float-right md:flex">
+        <motion.div
+          className="text-xs text-balance md:text-pretty md:text-xl md:float-right md:flex md:items-center"
+          initial={{ y: "20%", opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.9 }}
+        >
           <div className="hidden md:block md:w-[45%] cursor-pointer">
-            <div className="bg-mblue text-white-smoke px-4 py-3 w-max rounded-full active:bg-blue-900">
-              Download My CV.
-            </div>
+            <FramerMagnet>
+              <div>MY CV</div>
+            </FramerMagnet>
           </div>
           <div className="md:w-[50%] leading-5 md:leading-9">
             My name is Htet Aung Lwin. I'm from Yangon. I specialize in
             front-end development with{" "}
             <span className="underline decoration-wavy decoration-1 underline-offset-4 md:decoration-2 md:underline-offset-8 decoration-mblue">
-              React.js.
+              React.
             </span>{" "}
             <span className="md:inline hidden">
               I thrive in agile environments where clean code, collaboration,
@@ -26,7 +49,7 @@ const Hero = () => {
             </span>{" "}
             Ability to collaborate effectively with designers and engineers.
           </div>
-        </div>
+        </motion.div>
       </div>
     </section>
   );
